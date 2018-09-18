df537113fa6c91f722c47ea3faec6f995b68fd06
Learning Center <contact.nazmul.247@gmail.com>
2018-09-18 16:39:40 +0600

    Initial commit

---
+++ app/src/main/java/uddipan/com/updatetablayout_main/OneFragment.java
@@ -0,0 +1,37 @@
+package uddipan.com.updatetablayout_main;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+
+public class OneFragment extends Fragment {
+
+
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+
+
+
+
+        return view;
+
+
+    }
+
+}
